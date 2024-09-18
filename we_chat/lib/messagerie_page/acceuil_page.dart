dff4baeb4d6600579d22af504234e93bf24fd3d8
Godani05 <ezechiel.huego@epitech.eu>
2024-09-18 13:44:53 -0400

    [ADD] finish page acceuil message and begin chat_page

--- we_chat/lib/messagerie_page/acceuil_page.dart
+++ we_chat/lib/messagerie_page/acceuil_page.dart
@@ -36,9 +36,7 @@ class AcceuilPage extends StatelessWidget {
           MenuSection(),
           FavoriteSection(),
           Expanded(
-            child: Container(
-              color: Colors.pink,
-            ),
+            child: MessageSection(),
           ),
         ],
       ),
@@ -89,48 +87,17 @@ class MenuSection extends StatelessWidget {
 }
 
 class FavoriteSection extends StatelessWidget {
-   FavoriteSection({super.key});
+  FavoriteSection({super.key});
 
   // ignore: non_constant_identifier_names
   final List favoriteContacts = [
-    {
-      'name': "Idolo",
-      'profile': 'images/avatar/mafieu.jpg'
-
-    },
-    {
-      'name': "Idolo1",
-      'profile': 'images/avatar/mafieu2.jpg'
-
-    },
-    {
-      'name': "Idolo2",
-      'profile': 'images/avatar/mafieu3.jpg'
-
-    },
-    {
-      'name': "Idolo3",
-      'profile': 'images/avatar/mafieu4.jpg'
-
-    },
-    {
-      'name': "Women",
-      'profile': 'images/avatar/mafieu5.jpg'
-
-    },
-    {
-      'name': "madame",
-      'profile': 'images/avatar/mafieu6.jpg'
-
-    },
-    {
-      'name': "monsieur",
-      'profile': 'images/avatar/mafieu7.jpg'
-
-    },
-    
-
-    
+    {'name': "Idolo", 'profile': 'images/avatar/mafieu.jpg'},
+    {'name': "Idolo1", 'profile': 'images/avatar/mafieu2.jpg'},
+    {'name': "Idolo2", 'profile': 'images/avatar/mafieu3.jpg'},
+    {'name': "Idolo3", 'profile': 'images/avatar/mafieu4.jpg'},
+    {'name': "Women", 'profile': 'images/avatar/mafieu5.jpg'},
+    {'name': "madame", 'profile': 'images/avatar/mafieu6.jpg'},
+    {'name': "monsieur", 'profile': 'images/avatar/mafieu7.jpg'},
   ];
 
   @override
@@ -140,12 +107,9 @@ class FavoriteSection extends StatelessWidget {
       child: Container(
         padding: const EdgeInsetsDirectional.symmetric(vertical: 15),
         decoration: const BoxDecoration(
-          color: dWithe,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(40),
-            topRight: Radius.circular(40)
-          )
-        ),
+            color: dWithe,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40), topRight: Radius.circular(40))),
         child: Column(
           children: [
             Row(
@@ -156,11 +120,10 @@ class FavoriteSection extends StatelessWidget {
                   child: Text(
                     "Favorite contacts",
                     style: GoogleFonts.inter(
-                      //color: Colors.white60,
-                      color: dColor,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600
-                    ),
+                        //color: Colors.white60,
+                        color: dColor,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600),
                   ),
                 ),
                 const IconButton(
@@ -169,9 +132,10 @@ class FavoriteSection extends StatelessWidget {
                     //color: Colors.white60,
                     color: dColor,
                     size: 20,
-                  ), onPressed: null,
                   ),
-            ],
+                  onPressed: null,
+                ),
+              ],
             ),
             SingleChildScrollView(
               scrollDirection: Axis.horizontal,
@@ -189,8 +153,9 @@ class FavoriteSection extends StatelessWidget {
                             color: dColor,
                             shape: BoxShape.circle,
                           ),
-                          child: CircleAvatar(radius: 20,
-                          backgroundImage: AssetImage(favorite['profile']),
+                          child: CircleAvatar(
+                            radius: 20,
+                            backgroundImage: AssetImage(favorite['profile']),
                           ),
                         ),
                         const SizedBox(height: 6),
@@ -201,7 +166,6 @@ class FavoriteSection extends StatelessWidget {
                             fontSize: 14,
                             fontWeight: FontWeight.w600,
                           ),
-                        
                         )
                       ],
                     ),
@@ -211,6 +175,172 @@ class FavoriteSection extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MessageSection extends StatelessWidget {
+  MessageSection({super.key});
+
+  final List messages = [
+    {
+      'photoProfile': 'images/avatar/mafieu.jpg',
+      'nameUser': 'Godani',
+      'message': 'Bonjour',
+      'unRead': 0,
+      'heure': '19:19'
+    },
+    {
+      'photoProfile': 'images/avatar/mafieu.jpg',
+      'nameUser': 'Idolo',
+      'message': 'Bonne journée',
+      'unRead': 2,
+      'heure': '07:57'
+    },
+    {
+      'photoProfile': 'images/avatar/mafieu.jpg',
+      'nameUser': 'Stone',
+      'message': 'Cool',
+      'unRead': 5,
+      'heure': '01:09'
+    },
+    {
+      'photoProfile': 'images/avatar/mafieu.jpg',
+      'nameUser': 'Idolo',
+      'message': 'Bonjour! vous allez bien ?',
+      'unRead': 0,
+      'heure': '19:19'
+    },
+    {
+      'photoProfile': 'images/avatar/mafieu.jpg',
+      'nameUser': 'Idolo',
+      'message': 'Occupé, on discute après.',
+      'unRead': 2,
+      'heure': '07:57'
+    },
+    {
+      'photoProfile': 'images/avatar/mafieu.jpg',
+      'nameUser': 'Monsieur',
+      'message': 'Bonjour Comment puis-je faire ?',
+      'unRead': 3,
+      'heure': '01:09'
+    },
+    {
+      'photoProfile': 'images/avatar/mafieu.jpg',
+      'nameUser': 'Mr Chat',
+      'message': 'Comment vous rencontrer ?',
+      'unRead': 3,
+      'heure': '01:09'
+    },
+    {
+      'photoProfile': 'images/avatar/mafieu.jpg',
+      'nameUser': 'Drissa Corporation',
+      'message': 'Merci DG ',
+      'unRead': 1,
+      'heure': '09:09'
+    },
+    {
+      'photoProfile': 'images/avatar/mafieu.jpg',
+      'nameUser': 'Mael Yao ',
+      'message': 'Bonsoir !',
+      'unRead': 0,
+      'heure': '03:09'
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        
+        children: messages.map((message) {
+          return InkWell(
+            onTap: () {},
+            splashColor: dColor,
+            child: Container(
+                padding: const EdgeInsets.only(left: 30, right: 10, top: 15),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 62,
+                      height: 62,
+                      margin: const EdgeInsets.only(right: 23),
+                      decoration: BoxDecoration(
+                          color: dColor,
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage(
+                                message['photoProfile'],
+                              ))),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    message['nameUser'],
+                                    style: GoogleFonts.inter(
+                                      color: Colors.grey,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  Wrap(
+                                    children: [
+                                      Text(
+                                    message['message'],
+                                    style: GoogleFonts.inter(
+                                      color: Colors.black87,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                    ],
+                                  ),
+
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Text(message['heure']),
+                                  message['unRead'] != 0 ? Container(
+                                    padding: const EdgeInsets.all(5),
+                                    decoration:  const BoxDecoration(
+                                      color: dColor,
+                                      shape: BoxShape.circle
+                                    ),
+                                    child: Text(
+                                      message['unRead'].toString(),
+                                      style: GoogleFonts.inter(
+                                        color: dWithe,
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ): Container()
+                                ],
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+                          Container(
+                            color: Colors.grey[400],
+                            height: 0.5,
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                )),
+          );
+        }).toList(),
       ),
     );
   }
---
+++ we_chat/lib/messagerie_page/conv_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ConvPage extends StatelessWidget{
+  const ConvPage({super. key });
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
